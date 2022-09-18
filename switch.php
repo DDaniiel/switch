0f586c1862dce4c07eb441c5836a6998fd87346c
DDanieel <bessmertnyjdana@gmail.com>
2022-09-18 14:09:58 +0200

    switch

---
+++ switch.php
@@ -0,0 +1,29 @@
+<?php
+$day = (rand(1 ,13 ));
+switch ($day) {
+    case 1:echo "1 класс";
+        break;
+    case 2:echo "2 класс";
+        break;
+    case 3:echo "3 класс";
+        break;
+    case 4:echo "4 класс";
+        break;
+    case 5:echo "5 класс";
+        break;
+    case 6:echo "6 класс";
+        break;
+    case 7:echo "7 класс";
+        break;
+    case 8:echo "8 класс";
+        break;
+    case 9:echo "9 класс";
+        break;
+    case 10:echo "10 класс";
+        break;
+    case 11:echo "11 класс";
+        break;
+    case 12:echo "12 класс";
+        break;
+    default:echo "<span style = \" color: red;\">нет 13</span>";
+}
